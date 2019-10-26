3d8c3c5eefa3d2d79fea089fdac65fc5bedddccf
harisan17 <junior_hari@yahoo.co.uk>
2019-10-25 20:12:03 -0400

    changes3

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ app.post("/", function(req, res){
     res.redirect("/");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server started on port: 3000");
 });
